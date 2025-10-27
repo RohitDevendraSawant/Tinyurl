28118e82108c40be9c51f1e44501b37b72c9eb05
Rohit Devendra Sawant <rosawant51@gmail.com>
2025-10-27 19:48:08 +0530

    feat: added header and footer

---
+++ client/TinyUrl/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div>
+        <p className='text-gray-500 text-center p-2'>Made in love with India ❣️</p>
+    </div>
+  )
+}
+
+export default Footer
---
+++ client/TinyUrl/src/components/Header.jsx
@@ -0,0 +1,31 @@
+import Button from './Button';
+import { useNavigate } from 'react-router';
+import { useAuth } from '../context/authContextProvider';
+
+const Header = () => {
+  const navigate = useNavigate();
+  const { user, logout } = useAuth();
+
+  const handleLogout = () => {
+    logout();
+  }
+
+  return (
+    <div className='bg-gray-900 p-2'>
+      <div className='flex items-center justify-around h-18'>
+        <p className='font-extrabold text-3xl text-white'>TinyUrl</p>
+        {
+          !user ? (
+            <div className='flex gap-2 items-center'>
+              <Button onClick={() => navigate('/login')} btnText='Login' />
+              <Button onClick={() => navigate('/register')} btnText='Register' />
+            </div>
+          ) : 
+          <Button onClick={handleLogout} btnText='Logout'/>
+        }
+      </div>
+    </div>
+  )
+}
+
+export default Header;
