9ab5cb89a71cf4393bfacfc667b28adc1e018fdd
Rohit Devendra Sawant <rosawant51@gmail.com>
2025-10-28 09:19:28 +0530

    feat: minor UI enhancements

--- client/TinyUrl/src/components/ProtectedRoutes.jsx
+++ client/TinyUrl/src/components/ProtectedRoutes.jsx
@@ -7,7 +7,7 @@ const ProtectedRoutes = () => {
   if (loading) {
     return (
       <div className="flex items-center justify-center h-screen bg-gray-900 text-white">
-        <div className="text-center">
+        <div className="text-center flex flex-col items-center justify-center">
           <div className="animate-spin rounded-full h-16 w-16 border-b-4 border-white mb-4"></div>
           <p>Checking authentication...</p>
         </div>
